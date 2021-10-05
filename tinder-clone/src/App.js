5553325771e2b965b217e8ba4da8be68dc49f948
korekweis <korekweis@gmail.com>
2021-10-04 22:41:24 -0700

    setting up project

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TINDER</h1>
+      {/* Header */}
+      <Header />
+      {/* Tinder Cards */}
+      {/* Swipe Buttons */}
+    </div>
+  );
+}
+
+export default App;
